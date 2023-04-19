50bf6a3409d3ac3c83b9fc25cb43a52f6b21572a
Lucas Farias <lucasfnf@gmail.com>
2023-04-19 20:15:24 +0900

    Update build script to append to `PATH` instead of `PROTOC`

--- build.rs
+++ build.rs
@@ -1,7 +1,11 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Setting `PROTOC` with binary from `protoc_bin_vendored`
-    let path_buf = protoc_bin_vendored::protoc_bin_path()?;
-    std::env::set_var("PROTOC", path_buf.as_path());
+    // Adding to `PATH` binary from `protoc_bin_vendored`.
+    // `protoc_bin_vendored` works well on local machines, but not so much within `Docker`, so installing the binary is still needed.
+    let env_path = std::env::var("PATH")?;
+    let path_buf = protoc_bin_vendored::protoc_bin_path()?.parent().expect("Valid parent path").to_path_buf();
+    let new_env_path = vec![std::env::split_paths(&env_path).collect(), vec![path_buf], ].into_iter().flatten();
+    let os_path = std::env::join_paths(new_env_path)?;
+    std::env::set_var("PATH", os_path);
 
     // Compiling `.proto`s
     tonic_build::compile_protos("proto/authentication.proto")?;
